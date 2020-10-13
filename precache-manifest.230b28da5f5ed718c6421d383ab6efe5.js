f343037e07b89867ee67789e8ac54d5ae606a123
langdinggai <1845255483@qq.com>
2020-10-13 16:22:51 +0800

    Updates

---
+++ precache-manifest.230b28da5f5ed718c6421d383ab6efe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43144f71ecf2581d315f046bb7a134e4",
+    "url": "/forms/index.html"
+  },
+  {
+    "revision": "5c69a0049de55bb24bad",
+    "url": "/forms/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/forms/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forms/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c69a0049de55bb24bad",
+    "url": "/forms/static/js/main.6d1198d4.chunk.js"
+  },
+  {
+    "revision": "85e92708e10ab2b0abb5",
+    "url": "/forms/static/js/runtime-main.d4583c0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forms/static/media/logo.5d5d9eef.svg"
+  }
+]);
